0f6b7aab9c14d85c7635d356a4ed30d61cc875d2
LEIDY RODRIGUEZ <LEIDY RODRIGUEZ@leidy-PC>
2018-03-16 11:32:32 -0500

    Resuelto

--- src/palindromo/Palindromo.java
+++ src/palindromo/Palindromo.java
@@ -1,4 +1,3 @@
-
 package palindromo;
 
 import java.util.Scanner;
@@ -6,16 +5,30 @@ import java.util.Scanner;
 public class Palindromo {
 
     public static void main(String[] args) {
-        Scanner entrada = new Scanner (System.in);
         int n;
-        System.out.print("ingrese un numero: ");
-        n=entrada.nextInt();
-        int cifras= 0;    //esta variable es el contador de cifras
-            while(n!=0){             //mientras a n le queden cifras
-                    n = n/10;         //le quitamos el último dígito
-                   cifras++;          //sumamos 1 al contador de cifras
-            }
-            System.out.println("El número tiene " + cifras+ " cifras");
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Introducir numero entero:");
+        n = entrada.nextInt();
+        verificarN(n);
     }
-    
+
+    public static void verificarN(int n) {
+
+        int r, s = 0, k, cifras = 0;
+        k = n;
+
+        while (n != 0) {
+            r = n % 10;
+            s = s * 10 + r;
+            n = n / 10;
+            cifras++;
+        }
+        if (cifras == 5 && s == k) {
+            System.out.print("\nel numero invertido es :" + s);
+            System.out.println("\nes palindromo");
+        } else {
+            System.out.println("\nno es palindromo");
+        }
+    }
+
 }
